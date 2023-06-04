4597b6114fdc2d28ce0caed1bd0b29a27d2f8b15
Shawnsan <santiagoshawn@gmail.com>
2023-06-04 18:49:50 -0400

    init

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+import vue from '@vitejs/plugin-vue'
 import shopify from 'vite-plugin-shopify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [shopify()]
+  plugins: [shopify(),vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
+
