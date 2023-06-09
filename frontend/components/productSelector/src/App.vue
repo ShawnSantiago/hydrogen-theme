e9bf4e495a2ae02d099b6b093bfeb36b924aa73b
Shawnsan <santiagoshawn@gmail.com>
2023-06-09 09:15:28 -0400

    testing put vue

--- frontend/components/productSelector/src/App.vue
+++ frontend/components/productSelector/src/App.vue
@@ -1,19 +1,11 @@
 <script setup>
-// import HelloWorld from './components/HelloWorld.vue'
+import ProductCard from './components/ProductCard.vue'
 // import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <ProductCard msg="You did it!" />
   </main>
 </template>
 
---
+++ frontend/components/productSelector/src/components/ProductCard.vue
@@ -0,0 +1,61 @@
+<template>
+
+  <div class="relative">
+    <div class="grid snap-start gap-6 ">
+      <div class="aspect-[4/5] overflow-hidden rounded bg-primary/5">
+      
+          <img
+        
+            src="{{ card_product.featured_media | image_url: width: 533 }}"
+            sizes="320px"
+          
+            class="aspect-[inherit] h-full w-full object-cover object-center"
+         
+          >
+         
+      </div>
+      <div class="grid gap-1">
+        <h3
+          class="text-copy"
+        >
+          <a
+            href="{{ card_product.url }}"
+            id="CardLink-{{ section_id }}-{{ card_product.id }}"
+            class="after:absolute after:inset-0 after:z-10 after:content-['']"
+          >
+          </a>
+        </h3>
+
+        <!-- {%- render 'price', product: card_product -%} -->
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script >
+export default {
+  data() {
+    return {
+      loading: false,
+      post: null,
+      error: null,
+    }
+  },
+  created() {
+ 
+        this.fetchData()
+      
+    
+  },
+  methods: {
+    async fetchData() {
+      this.error = this.post = null
+      this.loading = true
+      console.log('fetching data')
+      return window.productData
+      
+    },
+  },
+}
+</script>
